fb622b512f4f3696a15e6513e24368c9cbf06646
Eino Palomäki <eino.palomaki@icloud.com>
2023-04-29 19:06:06 +0300

    project is nearly ready

--- backend/script.js
+++ backend/script.js
@@ -1,73 +1,5 @@
 "use strict"
 
-const fetchWindspeed = async() => {
-	try {
-		const response = await fetch('http://webapi19sa-1.course.tamk.cloud/v1/weather/wind_speed')
-		const data = await response.json();
-		console.log(data);
-		let tab = '';
-		data.forEach(function(item) {
-			const parsedDate = new Date(item.date_time);
-			const dateString = parsedDate.toLocaleDateString();
-			const timeString = parsedDate.toLocaleTimeString();
-
-			tab += `<tr>
-			<td>${dateString}</td>
-			<td>${timeString}</td>
-			<td>${item.wind_speed}</td>
-			</tr>`
-		});
-
-		document.getElementById('list-windspeed').innerHTML = tab;
-		
-		$(`#userTable`).dataTable({
-			"data": data.map,
-			"columns" : [
-				{"data" : 'Date'},
-				{"data" : 'Time'},
-				{"data" : 'WindSpeed'},
-			]
-		})
-
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-const fetchTemperature = async() => {
-	try {
-		const response = await fetch('https://webapi19sa-1.course.tamk.cloud/v1/weather/temperature')
-		const data = await response.json();
-		console.log(data);
-		let tab = '';
-		data.forEach(function(item) {
-			const date_time = item.date_time;
-			const parsedDate = new Date(date_time);
-			const dateString = parsedDate.toLocaleDateString();
-			const timeString = parsedDate.toLocaleTimeString();
-
-			tab += `<tr>
-			<td>${dateString}</td>
-			<td>${timeString}</td>
-			<td>${item.temperature}</td>
-			</tr>`
-		});
-		document.getElementById('list-temperature').innerHTML = tab;
-		
-		$(`#userTable`).dataTable({
-			"data": data.map,
-			"columns" : [
-				{"data" : 'Date'},
-				{"data" : 'Time'},
-				{"data" : 'Temperature'},
-			]
-		})
-
-	} catch (error) {
-		console.log(error);
-	}
-}
-
 const fetchLastValues = async() => {
 	try {
 		const response = await fetch('http://webapi19sa-1.course.tamk.cloud/v1/weather/limit/50')
@@ -103,20 +35,132 @@ const fetchLastValues = async() => {
 			</tr>`
 		});
 		document.getElementById('list-lastValues').innerHTML = tab;
-		
-		$(`#userTable`).dataTable({
-			"data": data.map,
-			"columns" : [
-				{"data" : 'Date'},
-				{"data" : 'Time'},
-				{"data" : 'Value'},
-			]
-		})
-
 	} catch (error) {
 		console.log(error);
 	}
 }
- fetchLastValues();
- fetchTemperature();
- fetchWindspeed();
+
+
+async function fetchData(signal, timespan, whichPage) {
+	if (timespan == undefined)
+		timespan = "";
+	if (signal == undefined)
+		signal = "temperature";
+	if (whichPage == undefined)
+		whichPage = 0;
+	try {
+		const response = await fetch(`https://webapi19sa-1.course.tamk.cloud/v1/weather/${signal}/${timespan}`)
+		const data = await response.json();
+		console.log(data);
+		let tab = '';
+		let datetimes= [];
+		data.forEach(function(item) {
+			const date_time = item.date_time;
+			const parsedDate = new Date(date_time);
+			const dateString = parsedDate.toLocaleDateString();
+			const timeString = parsedDate.toLocaleTimeString();
+			datetimes.push(dateString + " " + timeString);
+
+			tab += `<tr>
+			<td>${dateString}</td>
+			<td>${timeString}</td>
+			<td>${item[signal]}</td>
+			</tr>`
+		});
+		if (whichPage == 0)
+			document.getElementById('list-frontpage').innerHTML = tab;
+		else if (whichPage == 1)
+			document.getElementById('list-temperature').innerHTML = tab;
+		else if (whichPage == 2)	
+			document.getElementById('list-light').innerHTML = tab;
+
+		const values = data.map(item => item[signal])
+		console.log(values);
+		let element;
+		if (whichPage == 0)
+		element = 'frontpageGraph';
+		else if (whichPage == 1)
+		element = "tempGraph";
+		else if (whichPage == 2)
+		element = 'lightGraph';
+		let chartStatus = Chart.getChart(element);
+		if (chartStatus != undefined)
+  			chartStatus.destroy();
+		const context = document.getElementById(element).getContext('2d');
+		const myChart = new Chart(context, {
+		type: 'line',
+		data: {
+			labels: datetimes,
+			datasets: [{
+				label:signal,
+				data:values,
+				backgroundColor: "red",
+				borderColor: "red",
+				borderWidth: 3
+			}],
+		},
+		options: {
+			scales: {
+				y: {
+					suggestedMin: -10,
+				}
+			},
+			plugins:{
+				title:{
+					display: true,
+					text: signal
+				},
+				layout: {
+					padding: 10
+				}
+			}
+			}
+		});
+
+		const valuesRange = range(values);
+		tab = '';
+		tab = `
+		<tr><td> mean</td>
+		<td>${mean(values)}</td></tr>
+		<tr><td>Median</td>
+		<td>${median(values)}</td></tr>
+		<tr><td>Mode</td>
+		<td>${mode(values)}</td></tr>
+		<tr><td>Range</td>
+		<td>${valuesRange[0]} to ${valuesRange[1]}</td></tr>
+		<tr><td>Standard deviation </td>
+		<td>${standardDeviation(values)}</td></tr>
+		`
+		if (whichPage == 0)
+			document.getElementById('list-stats').innerHTML = tab;
+		if (whichPage == 1)
+			document.getElementById('list-tempStats').innerHTML = tab;
+		if (whichPage == 2)
+			document.getElementById('list-lightStats').innerHTML = tab;
+
+
+		} catch (error) {
+			console.log(error);
+	}
+}
+
+function handleDataChange(whichPage) {
+	let selectedSignal = 0;
+	let selectedTime = 0;
+	if (whichPage == 1){
+		selectedSignal = "temperature";
+		selectedTime = document.getElementById("tempSelect").value;
+	}
+	else if (whichPage == 2){
+		selectedSignal ="light";
+		selectedTime = document.getElementById("lightSelect").value;
+	}
+	else {
+		selectedSignal = document.getElementById("dataSelect").value;
+		selectedTime = document.getElementById("timeSelect").value;
+	}
+
+	fetchData(selectedSignal, selectedTime, whichPage);
+}
+
+fetchData();
---
+++ backend/statistics.js
@@ -0,0 +1,61 @@
+
+function mean(values) {
+	var total = 0;
+	
+	values.forEach(function (value) {
+		total += +value;
+	})
+	console.log(total);
+	return total / values.length
+}
+
+function median(values){
+
+	var nbrValues = values.map(Number)
+	nbrValues.sort(function(a,b){
+		return a - b;
+	});
+
+	var half = Math.floor(nbrValues.length / 2);
+
+	if(nbrValues.length % 2)
+		return nbrValues[half];
+	
+	return (nbrValues[half - 1] + nbrValues[half]) / 2.0;
+	
+}
+
+function mode(values) {
+	const mode = {};
+	var max = 0, count = 0;
+
+	for(var i = 0; i < values.length; i++)
+	{
+		const item = values[i]
+
+		if(mode[item])
+			mode[item] ++;
+		else
+			mode[item] = 1;
+
+		if(count < mode[item]){
+			max = item;
+			count = mode[item];
+		}
+	}
+	return max;
+}
+
+function range(values) {
+	
+	values.sort((a,b) => a - b);
+
+	return [values[0], values[values.length -1]]
+}
+
+function standardDeviation (values) {
+	var nbrValues = values.map(Number)
+	const n = nbrValues.length
+	const mean = nbrValues.reduce((a, b) => a + b) / n
+	return Math.sqrt(nbrValues.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
